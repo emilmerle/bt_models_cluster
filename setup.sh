76d934d1182a1088c9c6c49a7b47cde5c304b7c4
Emil Merle <emilmerle8@gmail.com>
2024-07-19 00:37:13 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -28,14 +28,3 @@ mkdir voc
 mv ../setup_data/VOC.yaml voc
 
 cd ..
-
-# Code
-cd code
-cd CNN_CIFAR10
-../.python-env/bin/python Training.py
-../.python-env/bin/python Inference.py
-cd ..
-cd CNN_FashionMNIST
-../.python-env/bin/python Training.py
-../.python-env/bin/python Inference.py
-# etc
