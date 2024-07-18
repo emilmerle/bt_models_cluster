3ff905ad85e388f057f1a9a66fd442e9eb57a78a
Emil Merle <emilmerle8@gmail.com>
2024-07-18 23:47:02 +0200

    New code

--- run_models.sh
+++ run_models.sh
@@ -2,11 +2,12 @@
 
 # Code
 cd code
+echo $(pwd)
 cd CNN_CIFAR10
-../.python-env/bin/python Training.py
-../.python-env/bin/python Inference.py
+./../../.python-env/bin/python Training.py
+./../../.python-env/bin/python Inference.py
 cd ..
 cd CNN_FashionMNIST
-../.python-env/bin/python Training.py
-../.python-env/bin/python Inference.py
+./../../.python-env/bin/python Training.py
+./../../.python-env/bin/python Inference.py
 # etc
